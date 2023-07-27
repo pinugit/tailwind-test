9b6d576c596e15a16e84bb2d27738347040e032e
pinugit <pinuwork@gmail.com>
2023-07-27 17:44:55 +0530

    completed HomeSearch component(task: to made the code shorter and component reusable)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import HomeSearch from "./components/sidebar/HomeSearch";
 
 function App() {
-  return <HomeSearch />;
+  return <HomeSearch onSelectingItem={(item) => void} />;
 }
 export default App;
